e83c033d5e2fa3e60568c38fbdc91998bf8d8f2a
Keegan McRoberts <keegan.mcroberts@gmail.com>
2023-03-15 13:03:06 -0500

    configuring react router dom and layout of app

---
+++ client/src/components/home.js
@@ -0,0 +1,17 @@
+import steve from "/Users/keegan/Development/code/gamereviews/client/src/favicon-32x32.png"
+
+
+function Home({logo}) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://upload.wikimedia.org/wikipedia/en/e/e7/Steve_%28Minecraft%29.png" className="App-logo" alt="whoops" />
+          <p>
+             Hail Steve
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Home;
