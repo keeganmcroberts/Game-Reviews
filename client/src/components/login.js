74dc2ed5b7dde43ea67fa9bf851c166ba6f9b591
Keegan McRoberts <keegan.mcroberts@gmail.com>
2023-03-17 11:42:22 -0500

    login form

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,11 +1,43 @@
+import React, { useRef, useEffect, useState } from 'react';
+
 function Login(){
+
+    const [userProfile, setUserProfile] = useState({
+        username: "",
+        password: ""
+    })
+
+    const {username, password} = userProfile
+    
+    function handleChange(e){
+        const {name, value} = e.target
+
+        setUserProfile({...userProfile, [name]: value})
+    }
+
+    function login(e){
+        e.preventDefault()
+    }
+
+
+
+
     return(
-        <h1>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-             Login </h1>
+       <div className="login">
+           <br></br>
+           <br></br>
+           <br></br>
+           <h3 className="login-title">Login</h3>
+           <form>
+            <h5>username:</h5>
+            <input className="login-field" name="username" value={username} onChange={handleChange}></input>
+            <h5>password:</h5>
+            <input className="login-field" name="password" value={password} onChange={handleChange}></input>
+            <input type='submit'></input>
+           </form>
+
+
+       </div>
     )
 }
 
