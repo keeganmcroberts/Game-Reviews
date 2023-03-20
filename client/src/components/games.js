94b23f1ecf1df5d61d76c1f43601e03eab973352
Keegan McRoberts <keegan.mcroberts@gmail.com>
2023-03-20 13:25:02 -0500

    filtering API request by platform and setting platform games to state

--- client/src/components/games.js
+++ client/src/components/games.js
@@ -1,14 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Games(){
 
+
+    const [gameDB, setGameDB] = useState({})
+    const [ps5Games, setPs5Games] = useState([])
+
+
 useEffect(()=>{
 
-    fetch('https://api.rawg.io/api/games?key=9937c17ee7f344e0a27e3d66c7b454e3')
+    fetch('https://api.rawg.io/api/platforms?key=9937c17ee7f344e0a27e3d66c7b454e3')
     .then(r=>r.json())
-    .then(data=>console.log(data))
+    .then(data=>setGameDB(data))
     
 },[])
+
+ console.log("DB",gameDB)
+
+ console.log("RESULTS", gameDB.results)
+
+
+
+     if (gameDB.results){
+         gameDB.results.map(platform=>{
+             if (platform.name === 'PlayStation 5'){
+                 platform.games.map(eachGame=>{
+                     ps5Games.push(eachGame)
+                    })
+                }
+            })
+        }
+
+        
+ console.log("ps5 games:",ps5Games)
 
 
     return(
