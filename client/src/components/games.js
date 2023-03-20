94545c6a8336918bd15798573edde6d2d26aa1a5
Keegan McRoberts <keegan.mcroberts@gmail.com>
2023-03-20 12:53:05 -0500

    fetching from public video game API. signed up to receive monthly access token

--- client/src/components/games.js
+++ client/src/components/games.js
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 function Games(){
+
+useEffect(()=>{
+
+    fetch('https://api.rawg.io/api/games?key=9937c17ee7f344e0a27e3d66c7b454e3')
+    .then(r=>r.json())
+    .then(data=>console.log(data))
+    
+},[])
+
+
     return(
         <h1>
             <br></br>
